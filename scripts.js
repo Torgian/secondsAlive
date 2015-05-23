07a6d5d6e257d2983064644925c55056f79da470
LakshaJael <nihongotraveler@icloud.com>
2015-05-23 19:09:41 +0900

    ALIVE!

---
+++ scripts.js
@@ -0,0 +1,9 @@
+var secondsPerMin = 60;
+var minsPerHour = 60;
+var hoursPerDay = 24;
+var daysPerYear = 365;
+var secondsPerDay = secondsPerMin * minsPerHour * hoursPerDay;
+document.write('There are ' + secondsPerDay + ' seconds in a day. ');
+var yearsAlive = 32;
+var secondsPerDay = secondsPerDay * daysPerYear * yearsAlive;
+document.write('I have been alive for ' + secondsPerDay + ' seconds and counting!');
